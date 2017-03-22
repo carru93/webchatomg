0fdf546aecd1e50b962603c0aa82d788262aee32
Emanuele <ema.caruso2@gmail.com>
2017-03-22 15:32:50 +0100

    chat page created

--- public/funzioni_chat.js
+++ public/funzioni_chat.js
@@ -1,4 +1,36 @@
 $(document).ready(function(){
-				console.log(localStorage["username"]+" recived");
-				$("#username_title").append(localStorage["username"]);
+				
+				var char_scritti = 0;
+				$("#chars").text(char_scritti);
+			
+				var username = localStorage["username"];
+				console.log(username + " recived");
+				
+				$("#inputBox").focus();
+				
+				$("#username_title").append(username);
+				
+				$("#username_data").append(username);
+				
+				$("#color_selector").change(function(){
+					$("#username_title").css("color", $("#color_selector").val());	
+					$("mex").css("color", $("color_selector").val());
 				});
+				
+				$("#sendButton").click(function(){
+					var l = $("#inputBox").val();
+					char_scritti += l.length;
+					$("#chars").text(char_scritti);
+				});
+
+				$("#azzera").click(function(){
+					char_scritti = 0;
+					$("#chars").text(char_scritti);
+				});
+
+				$("#inputBox").keydown(function(e){
+					if(e.which === 13){
+						$('#sendButton').click();
+					}
+				});
+			});
